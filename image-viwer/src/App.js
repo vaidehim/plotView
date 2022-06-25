7330b1498b5a4e872e1622abcd2a76809aef951f
vaidehi <m.vaidehi19@gmail.com>
2022-06-25 11:41:45 +0530

    remove comments

--- image-viwer/src/App.js
+++ image-viwer/src/App.js
@@ -4,22 +4,6 @@ import ImageLoader from './components/imageLoader/imageloder';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
     <div>
       <ImageLoader></ImageLoader>
     </div>
